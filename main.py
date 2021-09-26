24db14a38e45c094b11de612576da7349994dbbb
egorcry <abulapin@mail.ru>
2021-09-26 12:16:27 +0300

    Finished programme

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 import os
 import random
-import time
 import neat
 
 pygame.font.init()
 
+# Game resolution
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
-global win
+# Variable for display on which gen we are
+GEN = 0
+
+# Initialization of screen
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
 pygame.display.set_caption('Flappy Birds')
 
+# Creating variables for sprites
 BIRD_IMGS = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('Images', 'bird1.png'))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('Images', 'bird2.png'))),
@@ -47,18 +51,32 @@ class Bird:
         self.height = self.y
 
     def move(self):
+        """
+        That's how the bird moving arc: is starts with speed -10.5 (if we jumped) pixels per minute(going up), and with
+        every tick it velocity decreasing by this formula. For example:
+        d(1) = -10.5*1+1.5*1 = -9 -> But it's -2 'cause of the condition (still going up);
+        ...;
+        d(7) = -10,5*7+1,5*49 = 0 (now the bird's velocity is 0);
+        d(8) = -10,5*8+1,5*64 = 12 (the bird starts going down);
+        If velocity is 0 from the start, than the speed of falling will increase, of course
+        :return: None
+        """
         self.tick_count += 1
 
         d = self.vel*self.tick_count + 1.5*self.tick_count**2
 
+        # Max speed of falling
         if d >= 16:
             d = 16
 
+        # Max speed of going up
         if d < 0:
             d -= 2
 
         self.y = self.y + d
 
+        # If bird is moving up from the init jump position we tilt it upward, and, when it's lower than init jump pos,
+        # we tilt it down
         if d < 0 or self.y < self.height + 50:
             if self.tilt < self.MAX_ROTATION:
                 self.tilt = self.MAX_ROTATION
@@ -69,6 +87,7 @@ class Bird:
     def draw(self, win):
         self.img_count += 1
 
+        # Creating the fly animation
         if self.img_count < self.ANIMATION_TIME:
             self.img = self.IMGS[0]
         elif self.img_count < self.ANIMATION_TIME*2:
@@ -81,15 +100,21 @@ class Bird:
             self.img = self.IMGS[0]
             self.img_count = 0
 
+        # If bird is going down than we need only spreaded wings sprite
         if self.tilt <= -80:
             self.img = self.IMGS[1]
             self.img_count = self.ANIMATION_TIME*2
 
+        # Rotating the bird
         rotated_image = pygame.transform.rotate(self.img, self.tilt)
         new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
         win.blit(rotated_image, new_rect.topleft)
 
     def get_mask(self):
+        """
+        Instead of doing the rectangle for collision we can create a mask which covers the sprite
+        :return: Object
+        """
         return pygame.mask.from_surface(self.img)
 
 
@@ -105,6 +130,7 @@ class Pipe:
         self.top = 0
         self.bottom = 0
 
+        # We flip the image of pipe not by X (False), but by Y (True)
         self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
         self.PIPE_BOTTOM = PIPE_IMG
 
@@ -124,6 +150,11 @@ class Pipe:
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
     def collide(self, bird):
+        """
+        Here we can check if the bird crashed into the pipes or not
+        :param bird: Object
+        :return: bool
+        """
         bird_mask = bird.get_mask()
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)
         bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
@@ -151,6 +182,11 @@ class Base:
         self.x2 = self.WIDTH
 
     def move(self):
+        """
+        In real we create two sprites of the base. And when first going from the right side, the second one replaces it.
+        It works with the second-one situation too.
+        :return: None
+        """
         self.x1 -= self.VEL
         self.x2 -= self.VEL
 
@@ -165,14 +201,20 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
 
     for pipe in pipes:
         pipe.draw(win)
 
-    text = STAT_FONT.render('Score: ' + str(score), True, (255, 255, 255))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+    scr = STAT_FONT.render('Score: ' + str(score), True, (255, 255, 255))
+    win.blit(scr, (WIN_WIDTH - 10 - scr.get_width(), 10))
+
+    gn = STAT_FONT.render('Generation: ' + str(gen), True, (255, 255, 255))
+    win.blit(gn, (10, 10))
+
+    brd = STAT_FONT.render('Birds: ' + str(len(birds)), True, (255, 255, 255))
+    win.blit(brd, (10, gn.get_height() + 10))
 
     base.draw(win)
 
@@ -183,24 +225,34 @@ def draw_window(win, birds, pipes, base, score):
 
 
 def main(genomes, config):
+    global GEN
+    GEN += 1
     nets = []
     ge = []
     score = 0
     birds = []
 
     for _, g in genomes:
+        # net is for controlling movements of each bird: net[0] => birds[0]
         net = neat.nn.FeedForwardNetwork.create(g, config)
         nets.append(net)
+
         birds.append(Bird(230, 350))
+
+        # g[0] => birds[0]
         g.fitness = 0
         ge.append(g)
 
     base = Base(730)
     pipes = [Pipe(700)]
+
+    # Exit-condition from the loop
     run = True
+
     clock = pygame.time.Clock()
 
     while run:
+        # FPS
         clock.tick(30)
 
         for event in pygame.event.get():
@@ -213,6 +265,8 @@ def main(genomes, config):
         pipe_ind = 0
 
         if len(birds) > 0:
+            # If birds already have passed the first pipe on the screen and there are two pipes at the same time
+            # then birds are working with the second one
             if len(pipes) > 1 and birds[0].x > pipes[0].PIPE_TOP.get_width() + pipes[0].x:
                 pipe_ind = 1
         else:
@@ -256,16 +310,22 @@ def main(genomes, config):
                 g.fitness += 5
             pipes.append(Pipe(600))
 
+        # Deleting the pipe that went out of the screen
         for r in rem:
             pipes.remove(r)
 
+        # We delete the bird if it's too high or too low
         for x, bird in enumerate(birds):
             if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
                 birds.pop(x)
                 nets.pop(x)
                 ge.pop(x)
 
-        draw_window(win, birds, pipes, base, score)
+        # We stop the programme after the bird reaches 50 scores
+        if score > 50:
+            break
+
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(config_path):
@@ -274,10 +334,12 @@ def run(config_path):
                                 config_path)
     p = neat.Population(config)
 
+    # After every gen we can see stats about this gen
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
+    # We start the main program for 50 times (generations)
     winner = p.run(main, 50)
 
 
